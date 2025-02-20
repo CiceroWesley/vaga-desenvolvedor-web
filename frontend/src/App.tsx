724b5ccad354e963ed933588f25f582d5bf4ebb5
Cicero Wesley <wesleycariutaba@gmail.com>
2025-02-20 13:40:06 -0300

    feat: initial setup frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import Form from "./components/Form"
+import { Container } from "./styled"
+
+function App() {
+
+
+  return (
+    <>
+      <Container>
+        <h1>Formulário de Contato</h1>
+        <Form />
+      </Container>
+    </>
+  )
+}
+
+export default App
